5a1b2a462edbf54d9235bd4c38959635ce6f4ffa
Bomb Atomically <ieugenelee@gmail.com>
2021-07-23 01:09:34 -0700

    added styling to liquor question

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+let liquorAnswer = document.querySelector('.liquor-btn');
+let liquorContainerEl = document.getElementById('card-container');
+let questions = []
+
+liquorAnswer.addEventListener('click', selectAnswer);
+
+function selectAnswer() {
+    console.log('Whiskey');
+};
+
