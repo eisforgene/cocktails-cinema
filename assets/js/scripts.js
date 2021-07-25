cf6dddae5b5abd61f439f728f0bcc4dafd02852f
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-24 20:04:29 -0700

    javascript additions movies

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+//let Liquor = document.getElementById('liquor');
+let tequila 
+let gin
+let 
+ 
+fetch(`http://www.omdbapi.com/?apikey=1c8371fd&i=tt0241527&s=${liquor}`)
+    .then(function (movieRes){
+        return movieRes.json();
+    }).then(function(movieData) {
+        console.log(movieData)
+    });
