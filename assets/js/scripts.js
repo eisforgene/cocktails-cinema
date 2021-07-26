283a6a2c85a968fac758f3807114683d91db6bc4
Bomb Atomically <ieugenelee@gmail.com>
2021-07-25 21:10:05 -0700

    added restart btn

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,40 +1,38 @@
-feature/moviejs
+// feature/moviejs
 //let Liquor = document.getElementById('liquor');
 //let tequila 
 //let gin
 //let 
  
-fetch(`http://www.omdbapi.com/?apikey=1c8371fd&i=tt0241527&s=${liquor}`)
-    .then(function (movieRes){
-        return movieRes.json();
-    }).then(function(movieData) {
-        console.log(movieData)
-    });
-
+// fetch(`http://www.omdbapi.com/?apikey=1c8371fd&i=tt0241527&s=${liquor}`)
+//     .then(function (movieRes){
+//         return movieRes.json();
+//     }).then(function(movieData) {
+//         console.log(movieData)
+//     });
 
   
 let liquorChoice = document.getElementById('answer-buttons');
-let liquorContainerEl = document.getElementById('liquor-selection');
-let questionContainerEl = document.getElementById('question-container');
+// let questionContainerEl = document.getElementById('liquor-container');
+// let refreshBtn = document.querySelector('restart-btn');
 
 // object.addEventListener(event, function)
 liquorChoice.addEventListener('click', liquorSelection); // call liquorSelection after click
 
-function liquorSelection() {
-    console.log('Started');
+function liquorSelection(event) {
+    event.preventDefault(); 
+    console.log(event.target.value); // target value within the button
 }
 
-function cocktailSelection () {
-
-}
-
-function restart () {
+function cocktailSelection() {
 
 }
 
-function displayMovie () {
+function displayMovie() {
 
 }
+
+
 
 
 
