876827a7bfb7b26aa418e3e119b46b11e0c8cdc4
Bomb Atomically <ieugenelee@gmail.com>
2021-07-27 00:36:27 -0700

    worked on retrieving cocktail information

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,7 +6,7 @@ function liquorSelection(event) {
     event.preventDefault();  
     console.log(event.target.value); // target value within the button
     const liquorAnswer = event.target.value; // store value in const variable
-    const urlString = `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${liquorAnswer}`; // create API string
+    const urlString = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${liquorAnswer}`; // create API string
     getCocktails(urlString); // invoke/call function with urlString as a parameter/argument
 }
 
@@ -15,17 +15,20 @@ function getCocktails(urlString) { //call function with urlString as parameter f
     .then(function(result) {
        return result.json();
     })
-    .then(function(data) {
-        console.log(data);
-        data.cocktails
-        const cocktails = data.result;
-        cocktails.forEach(cocktails => displayCocktails(cocktail)); 
-        displayCockatails(cocktails)
+    .then(function(result) {
+        console.log(result);
+        const cocktails = result.drinks[0].strDrinkThumb;
+
+        const reponseCardEl = document.querySelector('.cocktail-btn');
+        responseCardEl.innerHTML = '<button>' + cocktails + '</button>'
+        cocktails.forEach(function(cocktail) {
+            displayCocktails(cocktail);
+        }); 
     })
 }
 
 function displayCocktails(cocktails) {
-    // cocktail array of results
+    let searchTerm = cocktails.drinks[0].strDrinkThumb// cocktail array of results
     // create element with cocktail info
     // query selector parent element
     // append cocktail element to parent
