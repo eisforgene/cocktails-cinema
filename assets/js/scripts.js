d0b45783f62cd542d835202642be9331a9ef066c
Bomb Atomically <ieugenelee@gmail.com>
2021-07-27 18:24:56 -0700

    added code to fetch image and append to cocktail container

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,7 @@ let liquorChoice = document.getElementById('answer-buttons');
 liquorChoice.addEventListener('click', liquorSelection); // call liquorSelection after click
 
 function liquorSelection(event) {
-    event.preventDefault();  
+    event.preventDefault();
     console.log(event.target.value); // target value within the button
     const liquorAnswer = event.target.value; // store value in const variable
     const urlString = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${liquorAnswer}`; // create API string
@@ -13,24 +13,39 @@ function liquorSelection(event) {
 
 
 function getCocktails(urlString) { //call function with urlString as parameter from liquorSelection();
-    fetch(urlString) 
-    .then(function(result) {
-       return result.json();
-    })
-    .then(function(result) {
-        console.log(result);
-        const cocktails = result.drinks[0].strDrinkThumb;
+    fetch(urlString)
+        .then(function (result) {
+            return result.json();
+        })
+        .then(function (result) {
+            console.log(result.drinks[0]);
+           
+            const cocktails = result.drinks[0];
+            let cocktailDisplay = document.querySelector('#cocktail-answer');
+            let cocktailImage = document.createElement('img'); 
+            
+            cocktailImage.setAttribute('src', cocktails.strDrinkThumb);
 
-        const reponseCardEl = document.querySelector('.cocktail-btn');
-        responseCardEl.innerHTML = '<button>' + cocktails + '</button>'
-        cocktails.forEach(function(cocktail) {
-            displayCocktails(cocktail);
-        }); 
-    })
+            cocktailDisplay.appendChild(cocktailImage);
+            
+            
+
+
+            // const reponseCardEl = document.querySelector('.cocktail-btn');
+            // responseCardEl.innerHTML = '<button>' + cocktails + '</button>'
+            // cocktails.forEach(function (cocktail) {
+            //     displayCocktails(cocktail);
+            // });
+        })
 }
 
 function displayCocktails(cocktails) {
     let searchTerm = cocktails.drinks[0].strDrinkThumb// cocktail array of results
+    
+    let cocktailDisplay = document.querySelector('.answer-buttons');
+    cocktailDisplay.innerHTML = " ";
+    cocktailDisplay.appendChild();
+    
     // create element with cocktail info
     // query selector parent element
     // append cocktail element to parent
@@ -38,6 +53,7 @@ function displayCocktails(cocktails) {
 }
 
 function selectCocktail(cocktail) { // select cocktails -- do stuff
+}
 
 function cocktailSelection() {
 
@@ -48,40 +64,32 @@ function restart() {
 }
 
 function displayMovie() {
- main
+    main
 
 }
-
-function displayMovie() {
-
-}
-
-
 
 // function selectAnswer() {
 //     console.log('Whiskey');
 // };
 //Function to build out movie selection
 //function movieSearch( ){
-=======
-function movieSearch( ){
 
-fetch(`http://www.omdbapi.com/?apikey=1c8371fd&s=action`)
-//change s=action to s=${liquorSelection}
-    .then(function (movieRes) {
-        return movieRes.json();
-    }).then(function (movieData) {
-        console.log(movieData)
-        for (var i = 0; i > 6 ; i++){
-            console.log(movieData.search.search[i].title)
-            console.log(movieData.search[i].Poster)
-        }
-    });
-movieChoice.addEventListener.getElementById("click", movie-btn) //calls movie image 
-    //let movieTitle = 
-    //when image comes up it is clickable to go to amazom 
-    //https:www.amazom.com/s=movieTitle 
-//}
-=======
-}
-
+function movieSearch() {
+    fetch(`http://www.omdbapi.com/?apikey=1c8371fd&s=action`)
+        //change s=action to s=${liquorSelection}
+        .then(function (movieRes) {
+            return movieRes.json();
+        }).then(function (movieData) {
+            console.log(movieData)
+            for (var i = 0; i > 6; i++) {
+                console.log(movieData.search.search[i].title)
+                console.log(movieData.search[i].Poster)
+            }
+        });
+    function movieChoice() {
+        document.querySelectorAll(".movie-btn", el.click()); //calls movie image
+        //let movieTitle =
+        //when image comes up it is clickable to go to amazom
+        //https:www.amazom.com/s=movieTitle
+    }
+};
